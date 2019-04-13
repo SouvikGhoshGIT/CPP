cd76888e613a719f778b3eb61b8d3ef1b452f826
SouvikGhoshGIT <ece.gh4u@gmail.com>
2019-04-13 20:00:49 +0000

    patial commit

--- function-practice.cpp
+++ function-practice.cpp
@@ -1,10 +1,119 @@
 #include<iostream>
+#include<tuple>
 
 int fun(){
     return 3;
 }
+
+void pass_by_value(int x){
+
+    x=30;
+}
+
+void pass_by_reference(int *p){
+
+    *p=30;
+}
+
+void example(int);
+
+void example(int a=10){
+
+    std::cout << a << "\n";
+}
+//Default arguments are different from constant arguments as constant arguments can’t be changed whereas default arguments can be overwritten if required.
+void exam(int const a=5){
+
+    std::cout << a << "\n";
+    
+
+}
+/*
+
+Remember, inlining is only a request to the compiler, not a command. Compiler can ignore the request for inlining. Compiler may not perform inlining in such circumstances like:
+1) If a function contains a loop. (for, while, do-while)
+2) If a function contains static variables.
+3) If a function is recursive.
+4) If a function return type is other than void, and the return statement doesn’t exist in function body.
+5) If a function contains switch or goto statement.
+
+
+It is also possible to define the inline function inside the class. In fact, all the functions defined inside the class are implicitly inline. Thus, all the restrictions of inline functions are also applied here. If you need to explicitly declare inline function in the class then just declare the function inside the class and define it outside the class using inline keyword.
+For example:
+
+filter_none
+edit
+play_arrow
+
+brightness_4
+class S 
+{ 
+public: 
+    inline int square(int s) // redundant use of inline 
+    { 
+        // this function is automatically inline 
+        // function body 
+    } 
+}; 
+
+C++ compiler checks the argument types of inline functions and necessary conversions are performed correctly. Preprocessor macro is not capable for doing this. One other thing is that the macros are managed by preprocessor and inline functions are managed by C++ compiler.
+
+Remember: It is true that all the functions defined inside the class are implicitly inline and C++ compiler will perform inline call of these functions, but C++ compiler cannot perform inlining if the function is virtual. The reason is call to a virtual function is resolved at runtime instead of compile time. Virtual means wait until runtime and inline means during compilation, if the compiler doesn’t know which function will be called, how it can perform inlining?
+
+One other thing to remember is that it is only useful to make the function inline if the time spent during a function call is more compared to the function body execution time. An example where inline function has no effect at all:
+*/
+
+class inline_practice{
+
+public:
+    void show();
+};
+
+inline void inline_practice::show(){
+
+std::cout <<"calling from inline fucntion \n";
+} 
+
+void a(){
+    std::cout << "From void function \n";
+    return;
+}
+
+//A Void function can return another void function
+
+void b(){
+    return a();
+}
+
+
+//returning multiple vslue from a function 1)Using tuple/pair 2)using pointer 3)structue 4)array
+std::tuple<int,int,bool> multi_tuple(int n,int m){
+return std::make_tuple(n,m,true);
+};
+
+void multi_pointer(int a,int b,int* c){
+*c=a+b;
+};
 int main(){
     int a=fun();
     std ::cout << a <<"\n" ;
+    pass_by_value(a);
+    std ::cout << a <<"\n" ;
+    pass_by_reference(&a);
+    std ::cout << a <<"\n" ;
+    example(10);
+    example();
+    exam();
+    exam(2);
+    inline_practice ip;
+    ip.show();
+    b();
+    int p,q;
+    bool cc;
+    std::tie(p,q,cc)=multi_tuple(2,3);
+    std::cout << p << " " <<q <<" " <<cc <<"\n";
+    int sum;
+    multi_pointer(2,3,&sum);
+    std::cout << sum <<"\n";
 }
 
